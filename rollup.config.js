50dd8ceafd2df61bc760a11e4b11522ddd889ea3
misilelaboratory <misileminecord@gmail.com>
2022-11-09 19:35:51 +0900

    import some

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
 
