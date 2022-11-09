c19659b38d50a50c6d159aa94c18a7a7bd4cafa1
MisileLaboratory <misileminecord@gmail.com>
2022-11-09 19:49:23 +0900

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -9,9 +9,15 @@
 	let flyx = -40;
 </script>
 
-<h1 use:reveal={{ transition: "slide", reset: true, duration: 500 }}>MisileLaboratory</h1>
-<h1 use:reveal={{ transition: "slide", reset: true, duration: 500 }}>Malkuth Reverbation</h1>
-<h1 use:reveal={{ transition: "slide", reset: true, duration: 500 }}>Blank</h1>
+<box>
+	<h1 use:reveal={{ transition: "slide", reset: true, duration: 500 }}>MisileLaboratory</h1>
+</box>
+<box>
+	<h1 use:reveal={{ transition: "slide", reset: true, duration: 500 }}>Malkuth Reverbation</h1>
+</box>
+<box>
+	<h1 use:reveal={{ transition: "slide", reset: true, duration: 500 }}>Blank</h1>
+</box>
 <!--
 <projectstitle use:reveal={{ transition: "slide", reset: true, duration: 1700}}>Projects</projectstitle>
 <details id="details" use:reveal={{ transition: "slide", reset: true, duration: 2500}} bind:open={visible["projects2021"]}>
